67e206201693019c351b11fc77b424ebe037bb89
drblessings <oladunjoyebabafemi@gmail.com>
2022-09-22 01:59:01 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -28,7 +28,7 @@ char *cap_string(char *str)
 
 	while (str[++index])
 	{
-		while (!(str[index] >= 'a') && (str[index] <= 'z'))
+		while (!(str[index] >= 'a' && (str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||
@@ -44,7 +44,9 @@ char *cap_string(char *str)
 				str[index - 1] == ')' ||
 				str[index - 1] == '{' ||
 				str[index - 1] == '}')
+				index == 0)
 			str[index] -= 32;
+		index++;
 	}
 
 	return (str);
