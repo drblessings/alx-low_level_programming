a31566548683c7d359d1049ca6cf567f555e7d7f
drblessings <oladunjoyebabafemi@gmail.com>
2022-10-03 09:49:22 -0700

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	for (index = 0; str[index]: index++)
+	for (index = 0; str[index]; index++)
 		len++;
 	cpy = malloc(sizeof(char) * (len + 1));
 
